9a1a1872d93d5171a2e92a7cc64ee682d565f5b0
npguy1 <46366363+npguy1@users.noreply.github.com>
2019-01-15 18:57:14 +0500

    add patient

--- Patient_App_v1/src/js/signUp.js
+++ Patient_App_v1/src/js/signUp.js
@@ -1,28 +1,69 @@
 
 // Get DOM elements
 
-let txtNewName = document.getElementById("txtNewName");
-let txtNewEmail = document.getElementById("txtNewEmail");
-let txtNewPassword = document.getElementById("txtNewPassword");
+let txtFullName = document.getElementById("txtFullName");
+let txtContactNumber = document.getElementById("txtContactNumber");
+let txtEmergencyNumber = document.getElementById("txtEmergencyNumber");
 
-let linkSignIn = document.getElementById("linkSignIn");
-let NewUserDiv = document.getElementById("NewUserDiv");
+let txtDob = document.getElementById("txtDob");
+let txtGender = document.getElementsByName("txtGender");
+let txtMaritalStatus = document.getElementsByName("txtMaritalStatus");
+let txtCity = document.getElementById("txtCity");
+let txtAddress = document.getElementById("txtAddress");
+let txtPersonHeight = document.getElementById("txtPersonHeight");
+let txtPersonWeight = document.getElementById("txtPersonWeight");
+let txtMedicalCondition = document.getElementById("txtMedicalCondition");
 
-let btnSignUp = document.getElementById("btnSignUp");
+let btnAddPatient = document.getElementById("btnAddPatient");
 
 
 
 
 //Add SignUp Event
-btnSignUp.addEventListener("click", e => {
-    var email = txtNewEmail.value;
-    var password = txtNewPassword.value;
-    var username = txtNewName.value;
-    console.log(email + "---" + password + "---" + username);
-    SignUp(email, password, username);
+btnAddPatient.addEventListener("click", e => {
+    var fullname = txtFullName.value;
+    var contactnumber = txtContactNumber.value;
+    var emergencynumber = txtEmergencyNumber.value;
+    var dob = txtDob.value;
+   // var gender = txtGender.value;
+    var maritalstatus = txtMaritalStatus.value;
+
+    for (var i = 0; i < txtGender.length; i++) {
+      if (txtGender[i].checked) {
+        var gender =  txtGender[i].value;
+      }
+  }
+
+  
+  for (var i = 0; i < txtMaritalStatus.length; i++) {
+    if (txtMaritalStatus[i].checked) {
+      var maritalstatus =  txtMaritalStatus[i].value;
+    }
+}
+
+/* 
+    if (document.getElementsByName('gender').checked) {
+      var gender = txtGender.value;;
+    }
+    if (document.getElementsByName('txtMaritalStatus').checked) {
+      var maritalstatus = txtMaritalStatus.value;
+    } */
+    var city = txtCity.value;
+    var address = txtAddress.value;
+    var personheight = txtPersonHeight.value;
+    var personweight = txtPersonWeight.value;
+    var medicalcondition = txtMedicalCondition.value;
+
+
+    console.log(fullname + "-" + contactnumber + "-" + emergencynumber + "-" + dob + "-" + gender + "-" + maritalstatus + "-" + city + "-" + address + "-" + personheight + "-" + personweight + "-" + medicalcondition);
+    addPatient(fullname,contactnumber,emergencynumber,dob,gender,maritalstatus,city,address,personheight,personweight,medicalcondition);
+    
+
+
+
   });
 
-  //SignIn Link Event
+/*   //SignIn Link Event
 linkSignIn.addEventListener("click", e => {
     // Hide Sign Up form Div
     NewUserDiv.classList.add("hide");
@@ -31,10 +72,12 @@ linkSignIn.addEventListener("click", e => {
     loggedOutDiv.classList.remove("hide");
   });
 
-
+ */
   // Sign Up Function
 function SignUp(email, password, username) {
     // TODO : Add email validation
+
+    
   
     firebase
       .auth()
@@ -56,19 +99,27 @@ function SignUp(email, password, username) {
   }
 
   //Add user info in database
-function addUser(userName) {
-    var user = firebase.auth().currentUser;
-    var uid = user.uid;
-    var userEmail = user.email;
+function addPatient(fullname,contactnumber,emergencynumber,dob,gender,maritalstatus,city,address,personheight,personweight,medicalcondition) {
+    
     firebase
       .database()
       //Node level user and asssing user auth uid as key of this record
-      .ref("/user/" + uid)
+      .ref("patient/")
       // .child(uid)
       .set({
-        userId: uid,
-        email: userEmail,
-        uname: userName
+        fullname: fullname,
+        contactnumber: contactnumber,
+        emergencynumber: emergencynumber,
+        dob: dob,
+        gender: gender,
+        maritalstatus: maritalstatus,
+        city: city,
+        address: address,
+        personheight: personheight,
+        personweight: personweight,
+        medicalcondition: medicalcondition
+
+
       })
       .catch(function(error) {
         console.error("Error writing new user to Realtime Database:", error);
