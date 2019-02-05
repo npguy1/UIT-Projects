9c8becade27a70d7a22ca3c4ff6e3a05d75d0733
Farhan Khan home <writefarhan@gmail.com>
2019-02-05 19:55:42 +0500

    ui

--- Patient_App_v8/src/js/signOut.js
+++ Patient_App_v8/src/js/signOut.js
@@ -2,18 +2,24 @@
 let linkSignOut = document.getElementById("linkSignOut");
 
 function signOut() {
-  //Add SignOut Event
-  linkSignOut.addEventListener("click", e => {
-    firebase
-      .auth()
-      .signOut()
-      .then(function() {
-        // Sign-out successful.
-        console.log("User signed out successfully");
-        location.href = "index.html";
-      })
-      .catch(function(error) {
-        // An error happened.
-      });
+  
+  firebase
+  .auth()
+  .signOut()
+  .then(function() {
+    // Sign-out successful.
+    console.log("User signed out successfully");
+    location.href = "index.html";
+  })
+  .catch(function(error) {
+    // An error happened.
   });
+
 }
+
+//Add SignOut Event
+linkSignOut.addEventListener("click", e => {
+
+  signOut();
+ 
+});
